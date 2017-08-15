b1d9567daf77cb9abd9440db13f49bffc601915a
chinabyte <3066902228@qq.com>
2017-08-15 16:37:24 +0800

    Initial Commit

---
+++ mustard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mustard
+//
+//  Created by chinabyte on 2017/8/15.
+//  Copyright © 2017年 chinabyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
